931e2a2ef4703e6f0945b598d16f841f8d41f3f9
leannefrank <leannfrank@gmail.com>
2021-06-07 23:30:34 +0530

    added react-router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
 import React from 'react';
+import {Switch, Route} from 'react-router-dom'
 
 function App() {
   return (
-    <div>
-         hello
-    </div>
+    <Switch>
+      <Route exact={true} path="/">
+
+        this is the home page
+
+      </Route>
+
+      <Route exact={true} path="/starred">
+
+        this is starred
+
+      </Route>
+
+      <Route>
+        error 404 page not found
+      </Route>
+
+    </Switch>
   );
 }
 
