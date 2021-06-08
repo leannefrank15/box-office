f3d52197570964c4dd6d9ea4fceeb6cc9b11b0e3
leannefrank <leannfrank@gmail.com>
2021-06-08 16:40:47 +0530

    created search bar and fetched the results from tv maze api

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,29 @@
-import React from 'react'
+import React, {useState} from 'react'
 import MainPageLayout from '../components/MainPageLayout'
 
 const Home = () => {
+
+  const [input_state, setInput] = useState(''); //creating state object
+
+  const onEventChange = (event_obj) => {
+    setInput(event_obj.target.value); //updating the state
+  }
+
+  const onSearch = () => {
+    fetch(`http://api.tvmaze.com/search/shows?q=${input_state}`).then(r => r.json()).then(result => {
+      console.log(result);
+    })
+  }
+
+  const onKeyDown = (event_obj) => {   //to make sure we can search by pressing enter key
+    if (event_obj.keyCode === 13){
+       onSearch();
+    }
+  }
   return (
     <MainPageLayout>
-      this is home!!
+     <input type='text' onChange={onEventChange} value={input_state} onKeyDown={onKeyDown} /> 
+     <button type='button' onClick={onSearch}>Search</button>
     </MainPageLayout>
   )
 }
