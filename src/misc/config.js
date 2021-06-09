405f47e6b817a98d18a6f5431e4f6b29e16ec6b3
leannefrank <leannfrank@gmail.com>
2021-06-10 02:03:59 +0530

    added loading and error messages to the show page

--- src/misc/config.js
+++ src/misc/config.js
@@ -2,6 +2,7 @@ const API_BASE_URL = 'https://api.tvmaze.com';
 
 export async function api_funct(queryString){
   const response = await fetch(`${API_BASE_URL}${queryString}`).then(r => r.json())
+
   return response;
   
 }
--- src/pages/Show.js
+++ src/pages/Show.js
@@ -7,15 +7,42 @@ const Show = () => {
   const {id} = useParams();
 
   const [show, setShow] = useState(null);
+  const [isLoading, setIsLoading]= useState(true);
+  const [error, setError] = useState(null);
+
   useEffect ( () => {
 
+    let isMounted = true;
+
      api_funct(`/shows/${id}?embed[]=seasons&embed[]=cast`).then(results => {
-       setShow(results);
-    });
+      
+        if(isMounted){
+        setShow(results);
+       setIsLoading(false)
+        }
+       
+      
+    }).catch(err => {
+      if (isMounted){
+      setError(err.message);
+      setIsLoading(false);
+      }
+    })
+
+    return () =>{
+      isMounted = false;
+    }
      
   }, [id] );
 
   console.log('show',show);
+
+  if(isLoading){
+    return <div>data is being loaded...</div>
+  }
+  if(error){
+    return <div>error occoured: (error) </div>
+  }
 
   return (
     <div>
