787e3d393a7e23d768915c1dfc34a5405e6c581b
leannefrank <leannfrank@gmail.com>
2021-06-09 12:49:42 +0530

    displayed show results and small fetch refactor

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,17 +1,20 @@
 import React, {useState} from 'react'
 import MainPageLayout from '../components/MainPageLayout'
+import {api_funct} from '../misc/config'
 
 const Home = () => {
 
   const [input_state, setInput] = useState(''); //creating state object
+  const [results, setResult] = useState(null); //creating state object for storing array of search results
 
   const onEventChange = (event_obj) => {
     setInput(event_obj.target.value); //updating the state
   }
 
   const onSearch = () => {
-    fetch(`http://api.tvmaze.com/search/shows?q=${input_state}`).then(r => r.json()).then(result => {
-      console.log(result);
+
+    api_funct(`/search/shows?q=${input_state}`).then(result => {
+      setResult(result);
     })
   }
 
@@ -20,10 +23,24 @@ const Home = () => {
        onSearch();
     }
   }
+
+  const displayResult = () => {
+    if (results && results.length === 0) {
+      return "no results found";
+    }
+    if (results && results.length >0){
+
+      return results.map((item) => <div key={item.show.id}>{item.show.name}</div>) 
+
+    }
+
+    return null;  // if neither of the conditions suffice , it'll return nothing
+  }
   return (
     <MainPageLayout>
      <input type='text' onChange={onEventChange} value={input_state} onKeyDown={onKeyDown} /> 
      <button type='button' onClick={onSearch}>Search</button>
+     <div><h4>{displayResult()}</h4></div>
     </MainPageLayout>
   )
 }
